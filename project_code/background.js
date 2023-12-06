b33d63d05de9509ff248ec5a72eb190b2104c872
Pratyusha Pogaru <pratyushapogaru@Pratyushas-MacBook-Air.local>
2023-12-05 16:10:12 -0800

    Updated Chrome extension to display results in a pop-up window

--- project_code/background.js
+++ project_code/background.js
@@ -2,23 +2,14 @@
 
 // Execute the function when the extension icon is clicked
 chrome.browserAction.onClicked.addListener(() => {
-  // console.log("Icon clicked");
 
   // Read the JSON file with sentiment analysis results
-  const fetchPromise = fetch(chrome.runtime.getURL('/sentiment_results.json'));
-  // console.log("Fetch Promise:", fetchPromise);
-
-  fetchPromise
-    .then(response => {
-      console.log("Response:", response);
-      return response.json();
-    })
+  fetch(chrome.runtime.getURL('/sentiment_results.json'))
+    .then(response => response.json())
     .then(data => {
-      console.log("Data:", data);
-
+      
       // Send the data to the content script
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        console.log("Sending data to content script");
         chrome.tabs.sendMessage(tabs[0].id, {
           action: "updateSentimentAnalysis",
           data: data
--- project_code/content.js
+++ project_code/content.js
@@ -2,16 +2,49 @@
 
 // Listen for a message from the background script
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.action === "updateSentimentAnalysis") {
-        updatePageWithSentimentAnalysis(request.data);
-      }
+  function(request, sender, sendResponse) {
+    if (request.action === "updateSentimentAnalysis") {
+      displayPopup(request.data);
     }
-  );
-  
-  // Function to update the webpage with sentiment analysis results
-  function updatePageWithSentimentAnalysis(data) {
-    // Implement logic to update the webpage here
-    console.log("Updating webpage with sentiment analysis results:", data);
   }
+);
+
+// Function to display sentiment analysis results in a popup dialog
+function displayPopup(data) {
   
+  // Extract predicted ratings
+  const predictedRatings = data.predicted_ratings;
+
+  // Calculate the average predicted rating
+  const averageRating = predictedRatings.reduce((sum, rating) => sum + rating, 0) / predictedRatings.length;
+
+  // Determine sentiment message based on the average rating
+  let sentimentMessage = "";
+  if (averageRating > 3.5) {
+    sentimentMessage = "Wow, this recipe has a lot of positive ratings!";
+  } else if (averageRating < 2.5) {
+    sentimentMessage = "Hmm, this recipe does not have many positive ratings.";
+  } else {
+    sentimentMessage = "This recipe has pretty neutral ratings.";
+  }
+
+  // Create a popup dialog
+  const popupContainer = document.createElement('div');
+  popupContainer.style.cssText = 'position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: #fff; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); z-index: 9999;';
+
+  // Display the average predicted rating and sentiment message in the popup
+  popupContainer.innerHTML = `
+    <h3>Predicted Overall Comment Rating</h3>
+    <p><strong>Average Comment Rating:</strong> ${averageRating.toFixed(2)}</p>
+    <p>${sentimentMessage}</p>
+    <button id="closePopupBtn">Close</button>
+  `;
+
+  // Append the popup to the body
+  document.body.appendChild(popupContainer);
+
+  // Close the popup when the close button is clicked
+  document.getElementById('closePopupBtn').addEventListener('click', function() {
+    document.body.removeChild(popupContainer);
+  });
+}
